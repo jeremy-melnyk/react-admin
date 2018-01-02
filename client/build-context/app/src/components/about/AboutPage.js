42983a9de726be69da42a5d7a489858b35f6d11c
Jeremy Melnyk <jeremyjm91@gmail.com>
2018-01-02 13:53:55 -0500

    Add About Component

---
+++ client/build-context/app/src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Add content here...</p>
+    </div>
+  );
+};
+
+export default AboutPage;
