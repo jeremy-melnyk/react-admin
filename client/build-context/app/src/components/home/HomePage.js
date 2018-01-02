45a7daa09711cc0ae270111959e71eb61eff5417
Jeremy Melnyk <jeremyjm91@gmail.com>
2018-01-02 13:54:07 -0500

    Add home component

---
+++ client/build-context/app/src/components/home/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>React Admin</h1>
+      <p>Add content here...</p>
+    </div>
+  );
+};
+
+export default HomePage;
