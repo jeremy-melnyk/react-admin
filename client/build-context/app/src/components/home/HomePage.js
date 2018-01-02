a482422e4ca5379b7dbd7d15095a02a8dd92e7b4
Jeremy Melnyk <jeremyjm91@gmail.com>
2018-01-02 14:13:30 -0500

    Change 'React Admin' to 'Code Puppet'

--- client/build-context/app/src/components/home/HomePage.js
+++ client/build-context/app/src/components/home/HomePage.js
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <div>
-      <h1>React Admin</h1>
+      <h1>Code Puppet</h1>
       <p>Add content here...</p>
     </div>
   );
