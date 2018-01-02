f29e2ac6400f2aa4c87663203080ca22d1573490
Jeremy Melnyk <jeremyjm91@gmail.com>
2018-01-02 13:53:36 -0500

    Update formatting index.js

--- client/build-context/app/src/index.js
+++ client/build-context/app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { render } from "react-dom";
-import { AppContainer } from "react-hot-loader";
-import configureStore, { history } from "./store/configureStore";
+import {render} from "react-dom";
+import {AppContainer} from "react-hot-loader";
+import configureStore, {history} from "./store/configureStore";
 import Root from "./components/Root";
 import "./styles/styles.scss";
 
@@ -12,19 +12,19 @@ const store = configureStore();
 
 render(
   <AppContainer>
-    <Root store={store} history={history} />
+    <Root store={store} history={history}/>
   </AppContainer>,
-  document.getElementById("app")
-);
+  document.getElementById("app"));
 
-if (module.hot) {
-  module.hot.accept("./components/Root", () => {
-    const NewRoot = require("./components/Root").default;
-    render(
-      <AppContainer>
-        <NewRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById("app")
-    );
-  });
+  if (module.hot) {
+    module
+      .hot
+      .accept("./components/Root", () => {
+        const NewRoot = require("./components/Root").default;
+        render(
+          <AppContainer>
+            <NewRoot store={store} history={history}/>
+          </AppContainer>,
+          document.getElementById("app"));
+      });
 }
